2abb4163c5cc43f40361c074a8189bd865ce8d68
Bryce Kehoe <bryce@kehoe.co.nz>
2019-01-11 11:32:32 +1300

    Added S01E05

---
+++ S01E05-1/express-demo/index.js
@@ -0,0 +1,47 @@
+const path = require('path')
+
+const express = require('express')
+const server = express()
+
+server.use(express.urlencoded({extended: true}))
+
+server.listen(3000, () => {
+  console.log('Listening on port 3000...')
+})
+
+server.get('/', (req, res) => {
+  console.log('Hit root route')
+  res.send('Hello world!')
+})
+
+server.get('/harakeke', (req, res) => {
+  console.log('Hit /harakeke')
+  res.send('<h1 style="color: red">Harakeke is awesome!</h1>')
+})
+
+server.get('/awesome/:name', (req, res) => {
+  console.log('Hit /awesome')
+  console.log(req.params)
+  res.send('Hi ' + req.params.name + ', you are awesome!')
+})
+
+server.get('/compliment', (req, res) => {
+  console.log('Hit /compliment')
+  console.log(req.query)
+  res.send('Welcome to the compliments page')
+})
+
+server.get('/faq', (req, res) => {
+  console.log(__dirname)
+  res.sendFile(path.join(__dirname, 'faq.html'))
+})
+
+server.get('/adduser', (req, res) => {
+  res.sendFile(path.join(__dirname, 'form.html'))
+})
+
+server.post('/adduser', (req, res) => {
+  console.log('Hit POST route at /adduser')
+  console.log(req.body)
+  res.send('Welcome new user! ' + req.body.email)
+})
