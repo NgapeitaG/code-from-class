9271ca309dd214887dd0956a9784789e57a0e009
Don Smith <don@antiantonym.com>
2019-01-29 10:21:04 +1300

    add S04E01 and S04E02

---
+++ S04E01-3/client/components/Animal.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Animal extends React.Component {
+  render () {
+    return (
+      <div className="animal">
+        This is the animal component
+      </div>
+    )
+  }
+}
+
+export default Animal
