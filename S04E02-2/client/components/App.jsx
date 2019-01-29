af11a3f113568339acce1843a9ac432f592c29e2
Don Smith <don@antiantonym.com>
2019-01-29 16:06:08 +1300

    add S04E02-2

---
+++ S04E02-2/client/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Colors from './Colors'
+
+const App = () => {
+  return (
+    <div>
+      <h1>Colours are cool!</h1>
+      <Colors />
+    </div>
+  )
+}
+
+export default App
