81be5d1500f80756747492660d323fd35865a87e
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-12 13:55:27 +1200

    greetings notes

--- S01E01/require-exports/greetings.js
+++ S01E01/require-exports/greetings.js
@@ -1,12 +1,13 @@
 function sayHello () {
   console.log('Hello!')
 }
-
+// outputs 'Hello' in console when function is called
 function sayGoodbye () {
   console.log('Goodbye!')
 }
-
+// outputs 'Goodbye' in console when function is called
 module.exports = {
   sayHello,
   sayGoodbye
 }
+// Exports functions
