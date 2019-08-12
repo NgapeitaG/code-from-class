cae989bb35094e8720449a026061db1c905fdeb0
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-12 17:02:35 +1200

    type of notes

--- S01E02-1/index.js
+++ S01E02-1/index.js
@@ -1,10 +1,16 @@
 const greetings = require('./greetings.js')
+//Creates a variable which grabs the greetings.js file with the hello and goodbye functions
 
 const hello = greetings.sayHello
+//This const will print 'Hello" in the console from the greetins const which requires the greetings file with the functions
 
 function runThisFunction (fn) {
   console.log(typeof fn)
   fn()
 }
+// runThisFunction is a function
+// console log fn will return the type of function. This will display function in the console.
+
 
 runThisFunction(hello)
+// The runThisFunction will call the hello const as it's parametre and will display 'Hello" in the console.  
