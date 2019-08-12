c885251ab1741a03779a1d9c7f060024ac8e05ea
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-12 13:50:39 +1200

    greetings notes

--- S01E01/require-exports/index.js
+++ S01E01/require-exports/index.js
@@ -1,4 +1,11 @@
 const greetings = require('./greetings')
 
 greetings.sayHello()
+// This executes the sayHello function within the greetings.js file
+// Outputs 'Hello' in the console
 greetings.sayGoodbye()
+// This executes the SayGoodbye function within the greetings.js file
+// Outputs 'Goodbye' in the console
+
+// const are variables which cannot be changed. They carry information
+// The const along with require means that the const greetings requires the greetings.js file
