b4ae21cbf84c94e871f9164413463d93abc1e5ed
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-12 19:16:52 +1200

    add function test notes

--- S01E03/add.test.js
+++ S01E03/add.test.js
@@ -1,19 +1,25 @@
 const add = require('./add')
+// requires the add.js file we're testing
 
 test('Test suite is running', () => {
   expect(true).toBe(true)
 })
+// Declaring test
 
 test('Test add function works for numbers', () => {
   // Arrange
   const num1 = 5
   const num2 = 4
+  // listing parametres or values
 
   const expected = 9
+  // Our expected outcome 
 
   // Act
   const actual = add(num1, num2)
+  // What is going on 
 
   // Assert
   expect(actual).toBe(expected)
 })
+// Asserting that we expect what we have to be the same as expected
