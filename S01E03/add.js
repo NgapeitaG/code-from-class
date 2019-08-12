2e4e2cadd1cb783841d31f6a44ae3e5177fc6dc4
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-12 19:10:26 +1200

    add function notes

--- S01E03/add.js
+++ S01E03/add.js
@@ -1,6 +1,10 @@
 function add (a, b) {
   return a + b
 }
+// The add function has parametres in brackets() of a & b. 
+// It will return the variables in the brackets added together.
+// add (3, 4) will return 7
+// There is no need to put function add, just add().
 
 module.exports = add
 // Enables an export of the add function to other files
