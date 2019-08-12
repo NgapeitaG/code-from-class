5c646d52be4ae5d64b82a730801ea072dfe4f36c
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-12 17:12:29 +1200

    type console log descriptions

--- S01E02-1/types.js
+++ S01E02-1/types.js
@@ -10,12 +10,12 @@ const types = {
   fn: function () { console.log('I am a function!') }
 }
 
-console.log('This is a ?: string', typeof types.str)
-console.log('This is a ?: boolean', typeof types.bool)
-console.log('This is a ?: object', typeof types.obj)
-console.log('This is a ?: object', typeof types.arr)
-console.log('This is a ?: number', typeof types.num)
-console.log('This is a ?: undefined', typeof types.und)
-console.log('This is a ?: null', typeof types.nul)
-console.log('This is a ?: NaN', typeof types.nan)
-console.log('This is a ?: function', typeof types.fn)
+console.log('This is a ?: string', typeof types.str) // Goes to the types const, looks at its str property and prints string in the console as the of this data
+console.log('This is a ?: boolean', typeof types.bool) //Prints as boolean typeof in console. Boolean is either true or false.
+console.log('This is a ?: object', typeof types.obj) // Objects have {} around them
+console.log('This is a ?: object', typeof types.arr) // Arrays have data listed in []
+console.log('This is a ?: number', typeof types.num) // Numbers are any numerical data
+console.log('This is a ?: undefined', typeof types.und) // Undefined means the type cannot be identified
+console.log('This is a ?: null', typeof types.nul) // Null means there is no identification for the type
+console.log('This is a ?: NaN', typeof types.nan) //Not a number. This indicates that a value is not a legal number
+console.log('This is a ?: function', typeof types.fn) // Function type
