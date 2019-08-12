8f6cf63d1899dc5bc6672736d1f82e8275d50b41
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-12 19:04:10 +1200

    const notes

--- S01E02-2/index.js
+++ S01E02-2/index.js
@@ -1,5 +1,6 @@
 const cowsay = require('cowsay')
 const colors = require('colors')
+// creates a const which requires the cowsay and colors dependancies
 
 console.log(colors.rainbow(cowsay.say({
   text: "I'm a moooodule",
