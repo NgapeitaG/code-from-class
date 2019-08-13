dd0b171d0db951777206e50041b7ac797ecf098a
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-13 18:54:51 +1200

    async vs sync notes

--- S02E01-1/notes.js
+++ S02E01-1/notes.js
@@ -1,3 +1,4 @@
 // An asynchronous function returns control to your code right away while it continues to do its work. 
 // This allows your code and the function to be working at the same time, asynchronously.
 // When your code calls a synchronous function, the function makes your code wait until it's finished before letting the next line run (also called blocking).
+// Look at the order difference of the data displayed in the console when you use node async.js and node sync.js in the terminal
