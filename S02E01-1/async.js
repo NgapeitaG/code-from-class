80cab4ae216b9c58f40ddbea9b713dd74026e892
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-13 17:24:50 +1200

    filepath notes

--- S02E01-1/async.js
+++ S02E01-1/async.js
@@ -1,11 +1,15 @@
 const fs = require('fs')
 const path = require('path')
+// requires 'fs' and 'path' packages 
 
 function getData (file, callback) {
   const filepath = path.join(__dirname, file)
+  // fs filepath creates a placeholder to the filepath of a file
+  // creates a const which makes a path to our file which is data.txt
 
   fs.readFile(filepath, 'utf8', callback)
 }
+// fs.readfile reads the entire contents of a file
 
 function showData (err, data) {
   if (err) {
