e2d1859ecd0b182b92b53c2a8bb7c885430afafc
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-13 16:46:02 +1200

    index.js notes

--- S01E05/notes.js
+++ S01E05/notes.js
@@ -1,2 +1,5 @@
 // faq.html opens an HTML webpage with three bullet points listing the words list item 
 // form.html open an HTML webpage with the placeholder 'email', an input box and a submit button
+// index.js requires the path and express dependancy
+// It lists the port we use, the server, where the server gets it data and where it posts the data to 
+// Index.js is the main javascript which handles our interactions with the HTML page
