d8391533e483c769d31db80dec46e2c9b7ef0745
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-13 16:43:13 +1200

    form html notes

--- S01E05/notes.js
+++ S01E05/notes.js
@@ -1,1 +1,2 @@
 // faq.html opens an HTML webpage with three bullet points listing the words list item 
+// form.html open an HTML webpage with the placeholder 'email', an input box and a submit button
