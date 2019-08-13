4fdc49b2ea01c0035c05379dae592a54a5aada33
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-13 18:47:36 +1200

    sync notes

--- S02E01-1/notes.js
+++ S02E01-1/notes.js
@@ -1,2 +1,3 @@
 // An asynchronous function returns control to your code right away while it continues to do its work. 
 // This allows your code and the function to be working at the same time, asynchronously.
+// When your code calls a synchronous function, the function makes your code wait until it's finished before letting the next line run (also called blocking).
