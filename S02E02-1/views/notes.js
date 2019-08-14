dab7a5d77fa6b5a333ecdb0e254006d18ac98eb8
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-14 12:22:30 +1200

    partials and layout notes

---
+++ S02E02-1/views/notes.js
@@ -0,0 +1,2 @@
+// home.hbs is the main handlebar which displays our data from different partials to our browser
+// to view all our partials and layouts into one, type yarn then yarn start in the terminal
