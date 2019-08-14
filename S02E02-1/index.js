d3330c29faa84fda4ba436afea55c8355d681aed
Ngapeita Leilua - Gotz <ngapeitaleilua-gotz@Ngapeitas-MacBook-Pro.local>
2019-08-14 12:07:21 +1200

    server notes

--- S02E02-1/index.js
+++ S02E02-1/index.js
@@ -6,3 +6,6 @@ server.listen(port, () => {
   // eslint-disable-next-line no-console
   console.log('Server is listening on port', port)
 })
+
+// requires our server and listens on the 3000 port in order for us to view the partials on our browser
+// Servers enable us to send our data to the browser
